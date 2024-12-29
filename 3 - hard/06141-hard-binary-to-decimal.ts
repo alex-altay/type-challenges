21c401b170d7fb2b68d06297c91461acf156f9f6
Alexander Trutanov <alexander.trutanov@libertexgroup.com>
2024-12-29 17:38:31 +0100

    Solves binary to decimal

---
+++ 3 - hard/06141-hard-binary-to-decimal.ts
@@ -0,0 +1,39 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from '../test-utils'
+
+type cases = [
+  Expect<Equal<BinaryToDecimal<'10'>, 2>>,
+  Expect<Equal<BinaryToDecimal<'0011'>, 3>>,
+  Expect<Equal<BinaryToDecimal<'00000000'>, 0>>,
+  Expect<Equal<BinaryToDecimal<'11111111'>, 255>>,
+  Expect<Equal<BinaryToDecimal<'10101010'>, 170>>,
+]
+
+
+// ============= Your Code Here =============
+type Map = {
+  1: [1],
+  2: [1, 1],
+  3: [1, 1, 1, 1],
+  4: [1, 1, 1, 1, 1, 1, 1, 1],
+  5: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  6: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  7: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+      1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  8: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+      1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+      1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+      1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+}
+
+type LengthOfString<T extends string, Acc extends 1[] = []> = 
+  T extends `${string}${infer Rest}`
+  ? LengthOfString<Rest, [...Acc, 1]>
+  : Acc['length']
+
+type BinaryToDecimal<T extends string, Acc extends 1[] = []> = 
+  T extends `${infer First}${infer Rest}`
+  ? First extends '1'
+    ? BinaryToDecimal<Rest, [...Map[LengthOfString<T> & keyof Map], ...Acc]>
+    : BinaryToDecimal<Rest, Acc>
+  : Acc['length']
